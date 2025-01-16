76a4cc91efeea96ae048d0cc4ef05ddfacf27a4e
Jerome Angibaud <djeangdev@yahoo.fr>
2025-01-17 00:00:56 +0100

    update

--- jeka-src/app/App.java
+++ jeka-src/app/App.java
@@ -2,16 +2,32 @@ package app;
 
 import dev.jeka.core.api.file.JkPathTree;
 import dev.jeka.core.tool.JkDep;
-
-import java.nio.file.Paths;
-
-// Declare here, production dependencies
+import picocli.CommandLine;
 
 @JkDep("dev.jeka:jeka-core:0.11.16")
-public class App {
+@JkDep("info.picocli:picocli:4.7.6")
+@JkDep("info.picocli:picocli-codegen:4.7.6")
+
+@CommandLine.Command(name = "checksum",
+        subcommands = { CommandLine.HelpCommand.class },
+        description = "Compute the checksum of the current dir.")
+public class App implements Runnable {
+
+    enum Algo {
+        MD5, SHA1, SHA256, SHA384, SHA512;
+    }
+
+    @CommandLine.Option(names = { "-a", "--algo" }, description = "Algorithm for checksum")
+    Algo algo = Algo.MD5;
 
     public static void main(String[] args) throws Exception {
-        String md5 = JkPathTree.of(Paths.get(".")).checksum("md5");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public void run() {
+        String md5 = JkPathTree.of(".").checksum(algo.toString().toLowerCase());
         System.out.println(md5);
     }
 
